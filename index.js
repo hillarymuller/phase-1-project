7211ab78f147ed36f8c69948ba50ad0361b4dc22
Hillary Muller <hillaryaward@gmail.com>
2022-06-20 11:29:32 -0500

    removed old code

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-//document.addEventListener('DOMContentLoaded', (category) => {
-  //  fetch(`http://localhost:3000/${category}`)
-    //.then(resp => resp.json())
-    //.then(category => console.log(category))
-//});
-
-//function fetchCategory(category) {
-  //  fetch(`http://localhost:3000/${category}`)
-    //.then(resp => resp.json())
-    //.then(category => console.log(category))
-//}
-
-
-
-
-
 
 const makeEl = element => document.createElement(element);
 const BASE_URL = 'http://localhost:3000/outfits'
@@ -96,37 +80,3 @@ function likeOutfit(thing){
 
 
 
-//function renderOutfit(outfits){
-  //  for (let category of outfits){
-    //    makeCategory(category)
-      //  console.log(Object.values(outfits))
-        //}
-        
-
-    
-//}
-//function makeCategory(category){
-    
-  //  const outfitCategory = document.getElementById(`${category}`);
-    //console.log(outfitCategory);
-    //const categoryCard = makeElement('div');
-    //const container = document.querySelector('.container');
-    //console.log(container);
-    
-    //categoryCard.append(outfitCategory);
-    //container.append(categoryCard);
-
-//}
-
-
-//  const classicAndCool = outfits[0];
-//  const trendyAndFun = outfits[1];
-// const comfyAndCasual = outfits[2];
-
-// const outfitCategory = makeElement('h3');
-// const outfitCard = makeElement('div');
-//  const outfitImg = makeElement('img');
-// const favoriteBttn = makeElement('button');
-// outfitCategory.textContent = outfits[i];
-// favoriteBttn.className = "favorite-bttn";
-// favoriteBttn.textContent = "&#x2661;";
